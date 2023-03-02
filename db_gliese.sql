c4f9bea1e9e326e5f427945e5ce4c592af76965a
Jeremi Gonzales <jeregr.21042002@gmail.com>
2023-03-02 11:57:50 -0500

    BD Actualizada

--- db_gliese.sql
+++ db_gliese.sql
@@ -1,132 +1,70 @@
-CREATE DATABASE  IF NOT EXISTS `db_gliese` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_gliese`;
--- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
---
--- Host: localhost    Database: db_gliese
--- ------------------------------------------------------
--- Server version	8.0.31
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `campus`
---
+DROP DATABASE IF EXISTS `db_gliese`;
+CREATE DATABASE IF NOT EXISTS `db_gliese` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_gliese`;
 
 DROP TABLE IF EXISTS `campus`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `campus` (
+CREATE TABLE IF NOT EXISTS `campus` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `campus`
---
-
-LOCK TABLES `campus` WRITE;
-/*!40000 ALTER TABLE `campus` DISABLE KEYS */;
-INSERT INTO `campus` VALUES (1,'CHANCAY',1),(2,'HUARAL',1),(3,'LIMA',1);
-/*!40000 ALTER TABLE `campus` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `categories`
---
+INSERT INTO `campus` (`id`, `description`, `status`) VALUES
+	(1, 'CHANCAY', 1),
+	(2, 'HUARAL', 1),
+	(3, 'LIMA', 1);
 
 DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categories` (
+CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(120) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (2,'VAMO A PROGRAMAR',1),(3,'VENDEDORGF',1);
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `document_type`
---
+INSERT INTO `categories` (`id`, `description`, `status`) VALUES
+	(2, 'VAMO A PROGRAMAR', 1),
+	(3, 'VENDEDORGF', 1);
 
 DROP TABLE IF EXISTS `document_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `document_type` (
+CREATE TABLE IF NOT EXISTS `document_type` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `document_type`
---
-
-LOCK TABLES `document_type` WRITE;
-/*!40000 ALTER TABLE `document_type` DISABLE KEYS */;
-INSERT INTO `document_type` VALUES (1,'DNI',1);
-/*!40000 ALTER TABLE `document_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `menu`
---
+INSERT INTO `document_type` (`id`, `description`, `status`) VALUES
+	(1, 'DNI', 1);
 
 DROP TABLE IF EXISTS `menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `menu` (
+CREATE TABLE IF NOT EXISTS `menu` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(80) NOT NULL,
   `icon` varchar(45) DEFAULT NULL,
   `order` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `menu`
---
-
-LOCK TABLES `menu` WRITE;
-/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'Home','home',1),(2,'Almacén','archive',2),(3,'Ventas','shopping-cart',3),(4,'Administración','sliders',4);
-/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `permission`
---
+INSERT INTO `menu` (`id`, `description`, `icon`, `order`) VALUES
+	(1, 'Home', 'home', 1),
+	(2, 'Almacén', 'archive', 2),
+	(3, 'Ventas', 'shopping-cart', 3),
+	(4, 'Administración', 'sliders', 4);
 
 DROP TABLE IF EXISTS `permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `permission` (
+CREATE TABLE IF NOT EXISTS `permission` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_role` int NOT NULL,
   `id_sub_menu` int NOT NULL,
@@ -137,51 +75,43 @@ CREATE TABLE `permission` (
   CONSTRAINT `FK_PERMISSION_ROLE` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_PERMISSION_SUB_MENU` FOREIGN KEY (`id_sub_menu`) REFERENCES `sub_menu` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `permission`
---
+INSERT INTO `permission` (`id`, `id_role`, `id_sub_menu`, `status`) VALUES
+	(1, 1, 1, 1),
+	(2, 1, 6, 1),
+	(3, 1, 7, 1),
+	(4, 1, 8, 1),
+	(5, 1, 9, 1),
+	(6, 1, 10, 1),
+	(7, 1, 11, 1);
 
-LOCK TABLES `permission` WRITE;
-/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
-INSERT INTO `permission` VALUES (1,1,1,1),(2,1,6,1),(3,1,7,1),(4,1,8,1),(5,1,9,1),(6,1,10,1),(7,1,11,1);
-/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `description` (`description`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 
---
--- Table structure for table `role`
---
+INSERT INTO `products` (`id`, `description`, `status`) VALUES
+	(1, 'PC GAMERs', 1),
+	(2, 'PORTATIL', 1),
+	(4, 'TECLADO', 1);
 
 DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role` (
+CREATE TABLE IF NOT EXISTS `role` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `role`
---
-
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'ADMINISTRADOR',1);
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sub_menu`
---
+INSERT INTO `role` (`id`, `description`, `status`) VALUES
+	(1, 'ADMINISTRADOR', 1);
 
 DROP TABLE IF EXISTS `sub_menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sub_menu` (
+CREATE TABLE IF NOT EXISTS `sub_menu` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_menu` int NOT NULL,
   `description` varchar(45) NOT NULL,
@@ -192,26 +122,18 @@ CREATE TABLE `sub_menu` (
   KEY `FK_SUB_MENU_MENU` (`id_menu`),
   CONSTRAINT `FK_SUB_MENU_MENU` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `sub_menu`
---
-
-LOCK TABLES `sub_menu` WRITE;
-/*!40000 ALTER TABLE `sub_menu` DISABLE KEYS */;
-INSERT INTO `sub_menu` VALUES (1,1,'Dashboards','circle','Dashboards',1),(6,2,'Productos','circle','Products',1),(7,2,'Categorias','circle','Categories',2),(8,3,'Clientes','circle','Clients',1),(9,4,'Usuarios','circle','Users',1),(10,4,'Roles','circle','Roles',2),(11,4,'Sedes','circle','Campus',3);
-/*!40000 ALTER TABLE `sub_menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
+INSERT INTO `sub_menu` (`id`, `id_menu`, `description`, `icon`, `url`, `order`) VALUES
+	(1, 1, 'Dashboards', 'circle', 'Dashboards', 1),
+	(6, 2, 'Productos', 'circle', 'Products', 1),
+	(7, 2, 'Categorias', 'circle', 'Categories', 2),
+	(8, 3, 'Clientes', 'circle', 'Clients', 1),
+	(9, 4, 'Usuarios', 'circle', 'Users', 1),
+	(10, 4, 'Roles', 'circle', 'Roles', 2),
+	(11, 4, 'Sedes', 'circle', 'Campus', 3);
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_role` int NOT NULL,
   `id_document_type` int NOT NULL,
@@ -233,27 +155,26 @@ CREATE TABLE `user` (
   KEY `FK_USER_DOCUMENT_TYPE` (`id_document_type`),
   CONSTRAINT `FK_USER_DOCUMENT_TYPE` FOREIGN KEY (`id_document_type`) REFERENCES `document_type` (`id`),
   CONSTRAINT `FK_USER_ROLE` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
 
---
--- Dumping data for table `user`
---
+INSERT INTO `user` (`id`, `id_role`, `id_document_type`, `first_name`, `last_name`, `document_number`, `address`, `telephone`, `email`, `user`, `password`, `image_url`, `status`, `active`) VALUES
+	(1, 1, 1, 'diego', 'uriarte chancafe', '74417614', 'Chancay', '913085587', 'grjere698@gmail.com', 'admin', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
+	(13, 1, 1, 'jeremi', 'gonzales', '72003668', 'Av. 1 de mayo 1031', '913085589', 'grjere698@gmail.com', 'admin2', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 0),
+	(15, 1, 1, 'ruben', 'dario', '72003669', 'Av. 1 de mayo 1031', '999888777', 'essaulherrerasangay601@gmail.com', 'admin3', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1);
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,1,1,'Diego','Uriarte Chancafe','74417614','Chancay','+51946977403',NULL,'admin','admin',NULL,1,1);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `user_attempts`;
+CREATE TABLE IF NOT EXISTS `user_attempts` (
+  `id_user` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `count` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_USER_ATTEMPTS_USER` (`id_user`),
+  CONSTRAINT `FK_USER_ATTEMPTS_USER` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
---
--- Table structure for table `user_campus`
---
 
 DROP TABLE IF EXISTS `user_campus`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_campus` (
+CREATE TABLE IF NOT EXISTS `user_campus` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
   `id_campus` int NOT NULL,
@@ -263,26 +184,22 @@ CREATE TABLE `user_campus` (
   KEY `FK_USER_CAMPUS_USER` (`id_user`),
   CONSTRAINT `FK_USER_CAMPUS_CAMPUS` FOREIGN KEY (`id_campus`) REFERENCES `campus` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_USER_CAMPUS_USER` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
 
---
--- Dumping data for table `user_campus`
---
+INSERT INTO `user_campus` (`id`, `id_user`, `id_campus`, `status`) VALUES
+	(1, 1, 1, 1),
+	(2, 1, 2, 1),
+	(16, 1, 3, 1),
+	(17, 13, 1, 1),
+	(18, 13, 2, 1),
+	(19, 13, 3, 1),
+	(20, 15, 1, 1),
+	(21, 15, 2, 1),
+	(22, 15, 3, 1);
 
-LOCK TABLES `user_campus` WRITE;
-/*!40000 ALTER TABLE `user_campus` DISABLE KEYS */;
-INSERT INTO `user_campus` VALUES (1,1,1,1),(2,1,2,1);
-/*!40000 ALTER TABLE `user_campus` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
 
--- Dump completed on 2022-12-07 21:51:14
