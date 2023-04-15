23f5c3534b2809945c1bbcebe9c40ea8de3e2776
Ruben Dario <rubendario7tu@gmail.com>
2023-04-15 10:30:01 -0500

    Apartado clientes

--- application/views/clients/index.php
+++ application/views/clients/index.php
@@ -29,6 +29,173 @@
                     </div>
                     <!-- /Header title-->
 
+                        <!-- Table -->
+                        <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <table class="table" id="datatable-clients">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Tipo de documento</th>.
+                                            <th>Número de documento</th>            
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
+                                            <th>Email</th>
+                                            <th>Razon social</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Table -->
+                    
+                    <!-- Create Supplier Modal -->
+                    <div class="modal fade" id="create_clients_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"> <!--  aria-hidden="true" -->
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header bg-transparent">
+                                    <button type="reset" class="btn-close reset" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body px-sm-5 pb-5">
+                                    <div class="text-center mb-2">
+                                        <h1 class="mb-1">Agregar nuevo cliente</h1>
+                                        <!-- <p data-i18n="Add new campus description">Permissions you may use and assign to your users.</p> -->
+                                    </div>
+                                    <form method="POST" enctype="multipart/form-data" id="create_clients_form" class="row" onsubmit="return false">
+                                        <div class="col-6">
+                                            <label class="form-label">Razón Social</label>
+                                            <input type="text" name="business_name" class="form-control" placeholder="Razón social" autofocus data-msg="" required />
+                                        </div>
+                                        <div class="col-6">
+                                            <label class="form-label">Nombre</label>
+                                            <input type="text" name="name" class="form-control" placeholder="Encargado" autofocus data-msg="" required />
+                                        </div>
+                                        <div class="col-12">
+                                            <div>
+                                                <label class="form-label">Tipo de documento</label>
+                                                <select name="document_type" class="form-select select2" data-msg="" required>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <label class="form-label">Nº De Documento</label>
+                                            <input type="number" name="document_number" class="form-control" placeholder="Nº De Documento" autofocus data-msg="" required />
+                                        </div>
+                                        <div class="col-12">
+                                            <div>
+                                                <label class="form-label">Email</label>
+                                                <input type="email" name="email" class="form-control" placeholder="user@example.com" data-msg="" />
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div>
+                                                <label class="form-label">Teléfono</label>
+                                                <input type="phone" name="phone" class="form-control" placeholder="Teléfono" data-msg="" />
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div>
+                                                <label class="form-label">Dirección</label>
+                                                <input type="address" name="address" class="form-control" placeholder="Dirección" data-msg="" />
+                                            </div>
+                                        </div>
+                                        <div class="col-12 text-center">
+                                            <button id="btn_create_clients" type="submit" class="btn btn-primary mt-2 me-1">Guardar</button>
+                                            <button type="reset" class="btn btn-outline-secondary mt-2 reset" data-bs-dismiss="modal" aria-label="Close">
+                                                <span>Cancelar</span>
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/ Create Supplier Modal -->
+
+
+                    <!-- Update User Modal -->
+                        <div class="modal fade" id="update_clients_modal"  data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+                         <div class="modal-dialog modal-lg modal-dialog-centered modal-edit-clients">
+                            <div class="modal-content">
+                                <div class="modal-header bg-transparent">
+                                    <button type="reset" class="btn-close reset" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body pb-5 px-sm-5 pt-50">
+                                    <div class="text-center mb-2">
+                                        <h1 class="mb-1">Actualizar cliente</h1>
+                                        <!-- <p>Updating clients details will receive a privacy audit.</p> -->
+                                    </div>
+
+                                    <form  method="POST" enctype="multipart/form-data" id="update_clients_form" class="row" onsubmit="return false">
+                                       
+                                        <!--/ header section -->
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Nombres</label>
+                                                <input type="text" name="name" class="form-control" placeholder="Nombres" data-msg="" required/>
+                                            </div>
+                                        </div>
+
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Tipo de documento</label>
+                                                <select name="document_type" class="form-select select2" data-msg="" required>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Número de documento</label>
+                                                <input type="number" name="document_number" class="form-control" placeholder="Número de documento" data-msg="" required/>
+                                            </div>
+                                        </div>
+
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Teléfono</label>
+                                                <input type="number" name="phone" class="form-control" placeholder="Teléfono" data-msg="" required/>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="mb-1 col-md-12">
+                                            <div>
+                                                <label class="form-label">Dirección</label>
+                                                <input type="text" name="address" class="form-control" placeholder="Dirección" data-msg=""/>
+                                            </div>
+                                        </div>
+
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Email</label>
+                                                <input type="email" name="email" class="form-control" placeholder="Email" data-msg="" required/>
+                                            </div>
+                                        </div>
+
+                                        <div class="mb-1 col-md-6">
+                                            <div>
+                                                <label class="form-label">Razon social</label>
+                                                <input type="text" name="business_name" class="form-control" placeholder="Razon social" data-msg="" required/>
+                                            </div>
+                                        </div>
+
+                                        <input type="hidden" name="id_clients">
+  
+                                        <div class="col-12 text-center mt-2 pt-50">
+                                            <button id="btn_update_clients" type="submit" class="btn btn-primary me-1">Guardar</button>
+                                            <button type="reset" class="btn btn-outline-secondary reset" data-bs-dismiss="modal">Cancelar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/ Update User Modal -->
+
                 </section>
                 <!-- Users ends -->
 
--- db_gliese.sql
+++ db_gliese.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             12.4.0.6659
+-- HeidiSQL Versión:             12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,10 +16,12 @@
 
 
 -- Volcando estructura de base de datos para db_gliese
+DROP DATABASE IF EXISTS `db_gliese`;
 CREATE DATABASE IF NOT EXISTS `db_gliese` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `db_gliese`;
 
 -- Volcando estructura para tabla db_gliese.campus
+DROP TABLE IF EXISTS `campus`;
 CREATE TABLE IF NOT EXISTS `campus` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
@@ -34,21 +36,42 @@ INSERT INTO `campus` (`id`, `description`, `status`) VALUES
 	(3, 'LIMA', 1);
 
 -- Volcando estructura para tabla db_gliese.categories
+DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(120) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
-  `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 -- Volcando datos para la tabla db_gliese.categories: ~2 rows (aproximadamente)
-INSERT INTO `categories` (`id`, `description`, `status`, `name`) VALUES
-	(2, 'VAMO A PROGRAMAR', 1, 'xD'),
-	(3, 'VENDEDORGF', 1, 'Sol');
+INSERT INTO `categories` (`id`, `description`, `status`) VALUES
+	(2, 'VAMO A PROGRAMAR', 1),
+	(3, 'VENDEDORGF', 1);
+
+-- Volcando estructura para tabla db_gliese.clients
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_document_type` int NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `document_number` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `phone` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `business_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `document_type` (`id_document_type`) USING BTREE,
+  CONSTRAINT `FK_CLIENTS_DOCUMENT_TYPE` FOREIGN KEY (`id_document_type`) REFERENCES `document_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+-- Volcando datos para la tabla db_gliese.clients: ~0 rows (aproximadamente)
+INSERT INTO `clients` (`id`, `id_document_type`, `name`, `document_number`, `address`, `phone`, `email`, `business_name`) VALUES
+	(1, 1, 'Ruben', '72131009', 'Chancay', '987975591', 'rubengmail.com', 'RYR');
 
 -- Volcando estructura para tabla db_gliese.detail_income
+DROP TABLE IF EXISTS `detail_income`;
 CREATE TABLE IF NOT EXISTS `detail_income` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_product` int NOT NULL,
@@ -64,6 +87,7 @@ CREATE TABLE IF NOT EXISTS `detail_income` (
 -- Volcando datos para la tabla db_gliese.detail_income: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla db_gliese.document_type
+DROP TABLE IF EXISTS `document_type`;
 CREATE TABLE IF NOT EXISTS `document_type` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
@@ -77,6 +101,7 @@ INSERT INTO `document_type` (`id`, `description`, `status`) VALUES
 	(2, 'RUC', 1);
 
 -- Volcando estructura para tabla db_gliese.income
+DROP TABLE IF EXISTS `income`;
 CREATE TABLE IF NOT EXISTS `income` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_supplier` int NOT NULL,
@@ -107,6 +132,7 @@ INSERT INTO `income` (`id`, `id_supplier`, `id_user`, `id_voucher_type`, `id_pay
 	(1, 1, 13, 1, 1, 'B001', '00000001', '2023-03-30 21:43:58', 0.18, NULL, NULL, 100.50, '1');
 
 -- Volcando estructura para tabla db_gliese.intent
+DROP TABLE IF EXISTS `intent`;
 CREATE TABLE IF NOT EXISTS `intent` (
   `id` int NOT NULL AUTO_INCREMENT,
   `token` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
@@ -127,9 +153,10 @@ INSERT INTO `intent` (`id`, `token`) VALUES
 	(21, 'ao4rLnLR32VGiEXTRJnRDvUa0/YeDi30TSIcPdbYLAdF8SS54edHQXF3yx6rCs3XBfuHr4C04kmqU9XJd5Ya5YlMZSdQDgCZTykvcHIrHGC+QrXzHtu8YLeshLb3W5pmQW5avw=='),
 	(22, '4M2cNr72yhLkmPpw+xXJt82moY7QeBgsAWNznGMkjnIbP3LrxA8OFdi3itOI9y38HC0rsQrgxKnE43AKUpVTTnRM/yUME4sFTUVKX/iWYvsYdkqcfh8P662f+Apoj0/chlz3Og=='),
 	(23, '4LDRDYaAaQu9fMJdJdpk43GN4uDk4tNzN6RZEhYdSJqSXlCSuDNPZA+wqVY5RVTw+qNyOVA+YbNjZrkXDMINumRg1st8sftzpcQvvp57tbDD3077aHoHnOP2CJ+78V8795lA1g=='),
-	(24, 'qQ82xrb5o3w/NUv8+4xU3QLIFSXYmLoFuXE4B8CQGn5vlKZRYBJaVRLyM6go8SAdHb0bSD6w/gARnwrZINKjOwYHjqpb5gTRDYxSsV1gxnzTknpZP2DT7G139Qbvi0uNpXY+6Q==');
+	(24, 'j6XJc5+hXo/dGRZXiRw7mpYbfeIuSdsyEtQU1uYxXUiu3Rw8Cs5KF3o5L0kxAigqmskr+EVi2iNe1qS2Swvt+BOdcXFesz5jVbjt8pFDRwbV/YPCPHeSvydu/Nx6AWGGyyPONw==');
 
 -- Volcando estructura para tabla db_gliese.menu
+DROP TABLE IF EXISTS `menu`;
 CREATE TABLE IF NOT EXISTS `menu` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(80) NOT NULL,
@@ -147,6 +174,7 @@ INSERT INTO `menu` (`id`, `description`, `icon`, `order`) VALUES
 	(5, 'Administración', 'sliders', 5);
 
 -- Volcando estructura para tabla db_gliese.payment_type
+DROP TABLE IF EXISTS `payment_type`;
 CREATE TABLE IF NOT EXISTS `payment_type` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(50) DEFAULT NULL,
@@ -161,6 +189,7 @@ INSERT INTO `payment_type` (`id`, `description`, `status`) VALUES
 	(3, 'Transferencia', 1);
 
 -- Volcando estructura para tabla db_gliese.permission
+DROP TABLE IF EXISTS `permission`;
 CREATE TABLE IF NOT EXISTS `permission` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_role` int NOT NULL,
@@ -190,6 +219,7 @@ INSERT INTO `permission` (`id`, `id_role`, `id_sub_menu`, `status`) VALUES
 	(20, 1, 17, 1);
 
 -- Volcando estructura para tabla db_gliese.products
+DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_category` int DEFAULT NULL,
@@ -218,6 +248,7 @@ INSERT INTO `products` (`id`, `id_category`, `description`, `stock`, `code`, `st
 	(97, 3, 'DESC 08', 80, '08', 1, NULL, 1);
 
 -- Volcando estructura para tabla db_gliese.role
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(45) NOT NULL,
@@ -230,6 +261,7 @@ INSERT INTO `role` (`id`, `description`, `status`) VALUES
 	(1, 'ADMINISTRADOR', 1);
 
 -- Volcando estructura para tabla db_gliese.sub_menu
+DROP TABLE IF EXISTS `sub_menu`;
 CREATE TABLE IF NOT EXISTS `sub_menu` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_menu` int NOT NULL,
@@ -259,6 +291,7 @@ INSERT INTO `sub_menu` (`id`, `id_menu`, `description`, `icon`, `url`, `order`) 
 	(17, 3, 'Nota de credito', 'circle', 'Creditnote', 5);
 
 -- Volcando estructura para tabla db_gliese.supplier
+DROP TABLE IF EXISTS `supplier`;
 CREATE TABLE IF NOT EXISTS `supplier` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_document_type` int NOT NULL,
@@ -266,7 +299,7 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   `document_number` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `address` varchar(150) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `phone` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `business_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `business_name` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `status` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`),
@@ -277,12 +310,13 @@ CREATE TABLE IF NOT EXISTS `supplier` (
 
 -- Volcando datos para la tabla db_gliese.supplier: ~4 rows (aproximadamente)
 INSERT INTO `supplier` (`id`, `id_document_type`, `name`, `document_number`, `address`, `phone`, `business_name`, `email`, `status`) VALUES
-	(1, 1, 'ALEXANDER', '71695889', 'lopez de zuñiga', '915959584', 'R&J ACTION', 'generateindollars@gmail.com', 1),
-	(3, 1, 'JEREMI', '12345678', 'Av 1 de mayo', '936672334', 'JYD ACTION', 'jeregr.21042002@gmail.com12', 1),
-	(4, 1, 'RUBEN DARIO', '12345679', 'Chancay Lopez 04', '987975591', 'R&amp;R ACTION', 'rubendario7tu@gmail.com', 1),
-	(5, 1, 'JEREMI ARMANDO GONZALES RUEDA', '71695880', 'Av 1 de mayo', '8', 'ACTION', 'jeregr.21042002@gmail.com', 1);
+	(1, 1, 'ALEXANDER', '71695889', 'lopez de zuñiga', '915959584', 'R&amp;J ACTION', 'generateindollars@gmail.com', 1),
+	(3, 1, 'JEREMI', '72003664', 'Av 1 de mayo', '936672334', 'J&amp;amp;D ACTION', 'jeregr.21042002@gmail.com12', 1),
+	(4, 1, 'RUBEN DARIO', '721368235', 'Chancay Lopez 04', '987975591', 'R&amp;R ACTION', 'rubendario7tu@gmail.com', 1),
+	(5, 1, 'JEREMI ARMANDO GONZALES RUEDA', '7', 'Av 1 de mayo', '8', 'J&amp;D ACTION ', 'jeregr.21042002@gmail.com', 1);
 
 -- Volcando estructura para tabla db_gliese.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_role` int NOT NULL,
@@ -305,16 +339,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   KEY `FK_USER_DOCUMENT_TYPE` (`id_document_type`),
   CONSTRAINT `FK_USER_DOCUMENT_TYPE` FOREIGN KEY (`id_document_type`) REFERENCES `document_type` (`id`),
   CONSTRAINT `FK_USER_ROLE` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3;
 
--- Volcando datos para la tabla db_gliese.user: ~4 rows (aproximadamente)
+-- Volcando datos para la tabla db_gliese.user: ~5 rows (aproximadamente)
 INSERT INTO `user` (`id`, `id_role`, `id_document_type`, `first_name`, `last_name`, `document_number`, `address`, `telephone`, `email`, `user`, `password`, `image_url`, `status`, `active`) VALUES
-	(1, 1, 2, 'Diego', 'Uriarte chancafe', '10123456781', 'Chancay', '913085587', 'grjere698@gmail.com', 'admin', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
+	(1, 1, 2, 'Diego', 'Uriarte chancafe', '12345678912', 'Chancay', '913085587', 'grjere698@gmail.com', 'admin', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
 	(13, 1, 1, 'Jeremi', 'Gonzales', '8', 'Av. 1 de mayo 1031', '913085589', 'grjere698@gmail.com', 'admin2', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
-	(15, 1, 1, 'Ruben', 'Dario', '4', 'Av. 1 de mayo 1031', '999888777', 'essaulherrerasangay601@gmail.com', 'admin3', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
-	(18, 1, 1, 'Alex', 'Diaz', '88888888', 'Calichera', '333333333333333333333333', 'generateindollars@gmail.com12', 'admin4', 'admin', NULL, 1, 1);
+	(15, 1, 1, 'Ruben', 'Dario', '12345678', 'Av. 1 de mayo 1031', '999888777', 'essaulherrerasangay601@gmail.com', 'admin3', '5a6d4d35597a41334e6a4a6a5a4459784d7a51355a6a457a596d593159324d7a597a566d4d5445784e7a633d', NULL, 1, 1),
+	(18, 1, 1, 'Alex', 'Diaz', '88888', 'Calichera', '8', 'generateindollars@gmail.com12', 'admin4', 'admin', NULL, 1, 1),
+	(19, 1, 1, 'Leonardo', 'Gonzales', '78945612', 'Av. 1 de mayo 1031', '987975591', 'grjere698@gmail.com', 'Leonardo', '4d47566d4e5463304d6a67774e5746695932557a5a54426a4d6d457a5a6a466b5a57517a4d7a63324e7a673d', NULL, 0, 1);
 
 -- Volcando estructura para tabla db_gliese.user_campus
+DROP TABLE IF EXISTS `user_campus`;
 CREATE TABLE IF NOT EXISTS `user_campus` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
@@ -325,9 +361,9 @@ CREATE TABLE IF NOT EXISTS `user_campus` (
   KEY `FK_USER_CAMPUS_USER` (`id_user`),
   CONSTRAINT `FK_USER_CAMPUS_CAMPUS` FOREIGN KEY (`id_campus`) REFERENCES `campus` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_USER_CAMPUS_USER` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb3;
 
--- Volcando datos para la tabla db_gliese.user_campus: ~12 rows (aproximadamente)
+-- Volcando datos para la tabla db_gliese.user_campus: ~15 rows (aproximadamente)
 INSERT INTO `user_campus` (`id`, `id_user`, `id_campus`, `status`) VALUES
 	(1, 1, 1, 1),
 	(2, 1, 2, 1),
@@ -340,9 +376,13 @@ INSERT INTO `user_campus` (`id`, `id_user`, `id_campus`, `status`) VALUES
 	(22, 15, 3, 1),
 	(23, 18, 1, 1),
 	(24, 18, 2, 1),
-	(25, 18, 3, 1);
+	(25, 18, 3, 1),
+	(26, 19, 2, 1),
+	(27, 19, 1, 1),
+	(28, 19, 3, 1);
 
 -- Volcando estructura para tabla db_gliese.voucher_type
+DROP TABLE IF EXISTS `voucher_type`;
 CREATE TABLE IF NOT EXISTS `voucher_type` (
   `id` int NOT NULL,
   `description` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
